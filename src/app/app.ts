d038c1e9bb89fa7a03411096ba88fe1857fb4ff2
unknown <casadullah49@gmail.com>
2025-09-03 01:37:41 +0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemsService } from './items.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  items: any[] = [];
+  newItem = '';
+
+  constructor(private service: ItemsService) {}
+
+  ngOnInit() {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.service.getItems().subscribe((data) => (this.items = data));
+  }
+
+  addItem() {
+    if (!this.newItem) return;
+    this.service.addItem({ name: this.newItem }).subscribe(() => {
+      this.loadItems();
+      this.newItem = '';
+    });
+  }
 }
---
+++ src/app/items.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ItemsService {
+  private apiUrl = 'http://50.19.52.6:5000/api/items'; // replace later with EC2 backend
+
+  constructor(private http: HttpClient) {}
+
+  getItems(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addItem(item: any): Observable<any> {
+    return this.http.post(this.apiUrl, item);
+  }
+}

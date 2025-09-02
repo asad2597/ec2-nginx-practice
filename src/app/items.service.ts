fdfdd2069ab3b83f74a7e1b997441f3d0956522a
unknown <casadullah49@gmail.com>
2025-09-03 02:36:13 +0500

    backend url updated

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ItemsService {
-  private apiUrl = 'http://50.19.52.6:5000/api/items'; // replace later with EC2 backend
+  private apiUrl = 'http://localhost:5000/api/items'; // replace later with EC2 backend
 
   constructor(private http: HttpClient) {}
 
